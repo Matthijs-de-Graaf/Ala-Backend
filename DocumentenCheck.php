0471b13f911756a863179a236ab65ba0fed80a6f
Matthijs-de-Graaf <117952288+Matthijs-de-Graaf@users.noreply.github.com>
2023-04-11 21:57:02 +0200

    e
    e

--- DocumentenCheck.php
+++ DocumentenCheck.php
@@ -1,25 +1,65 @@
-<!DOCTYPE html>
-<html lang="nl">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>DocuCheck</title>
-        <link rel="stylesheet" href="Ala.css">
-    </head>
-<body>
-    <header>
-        <img class="logo" src="DocuCheck.png ">
-            <nav>
-                <a class="white" href="Ala.php">Home</a>
-                <a class="white" href="Toevoegen.php">Toevoegen</a>
-                <a class="white" href="Retrieve.php">Vragenlijst</a>
-                <a class="white" href="Vragen-Beantwoorden.php">Beantwoorden</a>    
-                <a class="white" href="DocumentenCheck.php">DocuCheck</a>
-            </nav>
-    </header>
-    <main>
+<?php
+include_once "Database.php";
+// Handle save and delete actions
+if (isset($_POST['action']) && isset($_POST['id'])) {
+  $id = $_POST['id'];
+  if ($_POST['action'] == 'save') {
+    $query = "UPDATE documents SET saved_at = CURRENT_TIMESTAMP WHERE id = $id";
+    $result = $conn->query($query);
+    if (!$result) {
+      die('Failed to save document: ' . $conn->error);
+    }
+  } else if ($_POST['action'] == 'delete') {
+    $query = "DELETE FROM documents WHERE id = $id";
+    $result = $conn->query($query);
+    if (!$result) {
+      die('Failed to delete document: ' . $conn->error);
+    }
+  }
+}
 
-    </main>
-</body>
-</html>
+// Handle file upload
+if (isset($_FILES['file'])) {
+  $filename = $_FILES['file']['name'];
+  $tmpname = $_FILES['file']['tmp_name'];
+  $path = "uploads/$filename";
+  if (move_uploaded_file($tmpname, $path)) {
+    $query = "INSERT INTO documents (filename, path) VALUES ('$filename', '$path')";
+    $result = $conn->query($query);
+    if (!$result) {
+      die('Failed to save document: ' . $conn->error);
+    }
+    echo "File uploaded successfully.";
+  } else {
+    echo "Failed to upload file.";
+  }
+}
+
+// Retrieve a random document from the database
+$query = "SELECT * FROM documents WHERE saved_at IS NULL ORDER BY RAND() LIMIT 1";
+$result = $conn->query($query);
+if (!$result) {
+  die('Failed to retrieve document: ' . $conn->error);
+}
+
+// Display the document to the user
+if ($result->num_rows > 0) {
+  $row = $result->fetch_assoc();
+  $path = $row['path'];
+  $filename = $row['filename'];
+  echo "<img src='$path' alt='$filename'>";
+  echo "<br>";
+  echo "<button onclick='saveDocument({$row['id']})'>Yes</button>";
+  echo "<button onclick='deleteDocument({$row['id']})'>No</button>";
+} else {
+  echo "No documents found.";
+}
+
+// Close the database connection
+$conn->close();
+?>
+
+<form method="post" enctype="multipart/form-data">
+  <input type="file" name="file">
+  <button type="submit">Upload</button>
+</form>
