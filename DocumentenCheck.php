9c81027dc21d62df7b9221a02d765efa0391eaa4
RedAnil <anilsoekhoe1@gmail.com>
2023-04-12 12:42:24 +0200

    e
    e

--- DocumentenCheck.php
+++ DocumentenCheck.php
@@ -20,7 +20,6 @@
     </header>
     <main>
     <?php
-    session_start();
 // Set the paths for the "Keep" and "Delete" directories
 $keepDir = "uploads/keep/";
 $deleteDir = "uploads/delete/";
@@ -42,25 +41,10 @@ if (!file_exists($deleteDir)) {
 // Check if the "Keep" button was clicked
 if(isset($_POST['keep'])) {
   // Get the uploaded file information
-  if(isset($_SESSION['fileName'])){
-    $fileName =  ($_SESSION['fileName']);
-    $fileTmpName = $_SESSION['fileTmpName'];
-    $fileType =  $_SESSION['fileType'];
-    echo $_SESSION['fileName'];
-    echo '<br>';
-    echo  $_SESSION['fileTmpName'];
-    echo '<br>';
-    echo $_SESSION['fileType'];
-  } else {
-    $fileName = basename($_FILES["fileToUpload"]["name"]);
-    $fileTmpName = $_FILES["fileToUpload"]["tmp_name"];
-    $fileType = $_FILES["fileToUpload"]["type"];
-    echo $fileName;
-    echo '<br>';
-    echo  $fileTmpName;
-    echo '<br>';
-    echo $fileType;
-  }
+  $fileName = basename($_FILES["fileToUpload"]["name"]);
+  $fileTmpName = $_FILES["fileToUpload"]["tmp_name"];
+  $fileType = $_FILES["fileToUpload"]["type"];
+
 
   // Set the file path and name for the "Keep" folder
   $keepPath = $keepDir . $fileName;
@@ -76,15 +60,9 @@ if(isset($_POST['keep'])) {
 // Check if the "Delete" button was clicked
 if(isset($_POST['delete'])) {
   // Get the uploaded file information
-  if(isset($_SESSION['fileName'])){
-    $fileName =   $_SESSION['fileName'];
-    $fileTmpName = $_SESSION['fileTmpName'];
-    $fileType =  $_SESSION['fileType'];
-  } else {
-    $fileName = basename($_FILES["fileToUpload"]["name"]);
-    $fileTmpName = $_FILES["fileToUpload"]["tmp_name"];
-    $fileType = $_FILES["fileToUpload"]["type"];
-  }
+  $fileName = basename($_FILES["fileToUpload"]["name"]);
+  $fileTmpName = $_FILES["fileToUpload"]["tmp_name"];
+  $fileType = $_FILES["fileToUpload"]["type"];
 
 
   // Set the file path and name for the "Delete" folder
@@ -97,13 +75,7 @@ if(isset($_POST['delete'])) {
     echo "Sorry, there was an error uploading and marking for deletion your file.";
   }
 }
-if(isset($_POST['advies'])){
-  $_SESSION['fileName'] = basename($_FILES['fileToUpload']['name']);
-  $_SESSION['fileTmpName'] = $_FILES["fileToUpload"]["tmp_name"];
-  $_SESSION['fileType'] = $_FILES["fileToUpload"]["type"];
 
-  header('Location: Vragen-Beantwoorden.php');
-}
 // Display the uploaded photo from the "Keep" folder
 if(isset($_FILES['fileToUpload'])){
   $fileName = basename($_FILES["fileToUpload"]["name"]);
@@ -122,32 +94,28 @@ if(isset($_POST['unset'])){
   session_unset();
 }
 ?>
+<script>
+function smollWindow(){
+  window.open('Vragen-Beantwoorden.php','targetWindow',
+                                   `toolbar=no,
+                                    location=no,
+                                    status=no,
+                                    menubar=no,
+                                    scrollbars=yes,
+                                    resizable=yes,
+                                    width=SomeSize,
+                                    height=SomeSize`);
+  return false;
+}
+</script>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-  <h2>Select a photo to upload:</h2>
-  <?php
-  if(isset($_SESSION['fileName'])){
-    $name =$_SESSION['fileName'];
-    echo "<p>current file: $name</p>";
-    echo  $_SESSION['advice'];
-    echo '<br>';
-    echo '<p>you can still change the file type:</p>';
-    echo '<input type="file" name="fileToUpload">';
-  } else {
-    echo '<input type="file" name="fileToUpload">';
-  }
-  ?>
-  <br><br>
+  <h2>Select a photo to upload:</h2><br>
+  <input type="file" name="fileToUpload"><br><br>
   <input type="submit" name="keep" value="Keep">
-  <input type="submit" name="delete" value="Delete">
+  <input type="submit" name="delete" value="Delete"><br>
   <br>
-  <input type="submit" name='advies' value='Krijg advies'>
-</form>
-<?php
-?>
-<form method="post">
-  <h1>Dont forget to remove, session glitch fix thingymajingy button very epic!</h1>
-  <input type="submit" name='unset' value='session fix'>
+  <input type="button" name='advies' value='Krijg advies' onclick="smollWindow()">
 </form>
     </main>
 </body>
